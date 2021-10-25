bcc8e47f19768a9b710ff9c7725958942a56abcc
georgedmendoza <georgedmendoza13@gmail.com>
2021-10-25 12:29:44 -0500

    lift nav state to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 // import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery'
 
 function App() {
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
+  const [categories] = useState([
+    { name:'commercial', description:'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name:'portraits', description:'Portraits of people in my life' },
+    { name:'food', description:'Delicious delicacies' },
+    { name:'landscape', description:'Fields, farmhouse, waterfalls, and the beauty of nature' }
+]);
+
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+return (
+  <div>
+    <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+    ></Nav>
+    <main>
+      <div>
+        <Gallery></Gallery>
         <About></About>
-      </main>
-    </div>
+      </div>
+    </main>
+  </div>
   );
 }
 
