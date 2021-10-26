29294876aaaeceaf5abf495e0b046bf671c15b94
georgedmendoza <georgedmendoza13@gmail.com>
2021-10-26 11:22:33 -0500

    conditional styling and rendering

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -23,12 +24,21 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
-      <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-      </main>
+
+      {/* using ternary operator */}
+      {!contactSelected ? ( // if condition
+      // react fragments <>, like a div
+        <> 
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </>
+      ) : ( // else condition, show contact form
+          <ContactForm></ContactForm>
+      )}
+
     </div>
   );
 }
